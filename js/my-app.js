32e98d57ac781dbd37e09d69c435c0750bbfe941
RRRR <56125991+RHaykal@users.noreply.github.com>
2021-05-02 08:44:43 +0700

    Basic Template of the future app

--- js/my-app.js
+++ js/my-app.js
@@ -19,4 +19,16 @@ var app = new Framework7({
     // ... other parameters
   });
 
-  var mainView = app.views.create('.view-main');
+// create searchbar
+var searchbar = app.searchbar.create({
+  el: '.searchbar',
+  searchContainer: '.list',
+  searchIn: '.item-title',
+  on: {
+    search(sb, query, previousQuery) {
+      console.log(query, previousQuery);
+    }
+  }
+});
+
+var mainView = app.views.create('.view-main');
