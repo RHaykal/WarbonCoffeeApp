0ed8717d8cae0d6fcb634c7c4e9fa11ae20379de
RRRR <56125991+RHaykal@users.noreply.github.com>
2021-05-02 08:50:12 +0700

    Basic template of the future app

---
+++ Warbon App/js/my-app.js
@@ -0,0 +1,34 @@
+var app = new Framework7({
+    // App root element
+    root: '#app',
+    // App Name
+    name: 'My App',
+    // App id
+    id: 'com.myapp.test',
+    // Enable swipe panel
+    panel: {
+      swipe: false,
+    },
+    // Add default routes
+    routes: [
+      {
+        path: '/about/',
+        url: 'about.html',
+      },
+    ],
+    // ... other parameters
+  });
+
+// create searchbar
+var searchbar = app.searchbar.create({
+  el: '.searchbar',
+  searchContainer: '.list',
+  searchIn: '.item-title',
+  on: {
+    search(sb, query, previousQuery) {
+      console.log(query, previousQuery);
+    }
+  }
+});
+
+var mainView = app.views.create('.view-main');
