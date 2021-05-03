fed52c96c9a34857ade23dcf645e004c3f995217
RRRR <56125991+RHaykal@users.noreply.github.com>
2021-05-03 15:03:33 +0700

    Now with login Screen!

--- js/my-app.js
+++ js/my-app.js
@@ -1,3 +1,5 @@
+var $$ = Dom7;
+
 var app = new Framework7({
     // App root element
     root: '#app',
@@ -15,20 +17,41 @@ var app = new Framework7({
         path: '/about/',
         url: 'about.html',
       },
+      {
+        path: '/login-screen/',
+        componentUrl: './login-screen.html',
+      },
     ],
     // ... other parameters
   });
 
-// create searchbar
-var searchbar = app.searchbar.create({
-  el: '.searchbar',
-  searchContainer: '.list',
-  searchIn: '.item-title',
-  on: {
-    search(sb, query, previousQuery) {
-      console.log(query, previousQuery);
+  // create searchbar
+  var searchbar = app.searchbar.create({
+    el: '.searchbar',
+    searchContainer: '.list',
+    searchIn: '.item-title',
+    on: {
+      search(sb, query, previousQuery) {
+        console.log(query, previousQuery);
+      }
     }
-  }
-});
+  });
+
+    // DOM events for my-sheet sheet
+    $$('.my-sheet').on('sheet:open', function (e) {
+      console.log('my-sheet open');
+    });
+    $$('.my-sheet').on('sheet:opened', function (e) {
+      console.log('my-sheet opened');
+    });
+
+    // Create swipe-to-step Sheet
+    app.sheet.create({
+      el: '.my-sheet-swipe-to-step',
+      swipeToClose: true,
+      swipeToStep: true,
+      backdrop: true,
+    });
+
 
 var mainView = app.views.create('.view-main');
